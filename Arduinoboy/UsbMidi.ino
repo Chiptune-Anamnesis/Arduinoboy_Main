46d00e02abf26e1ef9a051e009e90914a11b3c5a
eggstoastbacon <eggstoastbacon@gmail.com>
2025-07-17 11:18:40 -0700

    Update UsbMidi.ino

--- Arduinoboy/UsbMidi.ino
+++ Arduinoboy/UsbMidi.ino
@@ -1,5 +1,4 @@
-
-#ifndef USE_TEENSY
+#if defined(USE_TEENSY) || defined(PRO_MICRO)
 void usbMidiSendTwoByteMessage(uint8_t b1, uint8_t b2) {};
 void usbMidiSendThreeByteMessage(uint8_t b1, uint8_t b2, uint8_t b3) {};
 void usbMidiSendRTMessage(uint8_t b) {};
